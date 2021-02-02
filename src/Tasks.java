ac0b031f63f6ec83a80146166de33b1fa19a3fe4
chikichunchik <andry010203@gmail.com>
2021-02-02 18:02:01 +0200

    Second task added

--- src/Tasks.java
+++ src/Tasks.java
@@ -3,14 +3,26 @@ import java.util.Iterator;
 import java.util.HashMap;
 public class Tasks {
 
-	public static void main(String[] args) {
-	}
 	public static boolean isActorWithoutMovie (DataBase dataBase){
         return  true;
     }
 
-    public static HashMap<Integer, Actor> moviePartners (Actor actor, DataBase dataBase){
-        return null;
+    public static HashMap<Integer, Actor> moviePartners (Actor actor){
+		try {
+			if (actor == null) { throw new IllegalArgumentException("Actor must not be null"); }
+			HashMap<Integer, Actor> partners = new HashMap<>();
+			for (Movie actorsMovie : actor.getMovies()) {
+				for (Actor partner : actorsMovie.getActors()) {
+					if (!partner.equals(actor)) {
+						partners.put(partner.hashCode(), partner);
+					}
+				}
+			}
+			return partners;
+		} catch (IllegalArgumentException error){
+			System.out.println("EXCEPTION! " + error.getMessage());
+			return null;
+		}
     }
 
     public static Movie maxActorsMovie (DataBase dataBase){
