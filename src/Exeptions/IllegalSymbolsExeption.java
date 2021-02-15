c912232bf7890d56ce428fcf2fbaf329d8341714
chikichunchik <andry010203@gmail.com>
2021-02-15 11:07:12 +0200

    serialVersionUID added

--- src/Exeptions/IllegalSymbolsExeption.java
+++ src/Exeptions/IllegalSymbolsExeption.java
@@ -1,6 +1,9 @@
 package Exeptions;
 
 public class IllegalSymbolsExeption extends Exception{
+    
+    private static final long serialVersionUID = -2445034564752834884L;
+
     public IllegalSymbolsExeption(String message) {
         super(message);
     }
