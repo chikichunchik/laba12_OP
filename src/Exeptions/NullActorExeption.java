0def861f966dd3e8072f9bd6c880c1b1e7676375
chikichunchik <andry010203@gmail.com>
2021-02-15 11:07:32 +0200

    NullActorExeption created

---
+++ src/Exeptions/NullActorExeption.java
@@ -0,0 +1,11 @@
+package Exeptions;
+
+public class NullActorExeption extends Exception{
+
+
+    private static final long serialVersionUID = -8619479105758095621L;
+
+    public NullActorExeption (String message){
+        super(message);
+    }
+}
